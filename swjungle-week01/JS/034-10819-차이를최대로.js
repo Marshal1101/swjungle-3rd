d23dc9b977163d60d07b374e6308572959302cd2
Marshal1101 <adequateplayer9@gmail.com>
2022-03-22 16:35:07 +0900

    완전탐색 진행중2

--- swjungle-week01/JS/034-10819-차이를최대로.js
+++ swjungle-week01/JS/034-10819-차이를최대로.js
@@ -3,12 +3,21 @@ const [N, input] = fs.readFileSync(
     '/home/chilling4u/Marshal1101/swjungle-3rd-s1-algorithm/swjungle-week01/JS/test.txt'
     ).toString().trim().split('\n');
 
-const result = new Array();
-let count = 0;
+
 function makeBigDifference(N, input) {
     const originList = input.split(' ');
     const caseList = new Array(N).fill(-1);
     const flagIndex = new Array(N).fill(0);
+    let count = 0;
+    let maxSum = 0;
+    
+    function sumList(N, caseList, originList) {
+        let ans = 0;
+        for (let i = 0; i < N-1; i++) {
+            ans += Math.abs(originList[caseList[i]] - originList[caseList[i+1]]);
+        }
+        return ans;
+    }
 
     function makeCase(i, originList) {
         for (let j = 0; j < N; j++) {
@@ -16,8 +25,10 @@ function makeBigDifference(N, input) {
                 caseList[i] = j;
                 if (i === N-1) {
                     count += 1;
-                    console.log('flagIndex', flagIndex, 'caseList', caseList);
-                    console.log(result.push(caseList));
+                    const sum = sumList(N, caseList, originList);
+                    if (sum > maxSum) {
+                        maxSum = sum;
+                    }
                 }
                 else {
                     flagIndex[j] = true;
@@ -29,13 +40,12 @@ function makeBigDifference(N, input) {
     }
 
     makeCase(0, originList);
-    console.log(result[0] === result[700] ? 1 : 0)
+    console.log(maxSum);
 }
 
 makeBigDifference(N, input);
-console.log('result', result[0], result[719], 'count', count);
 
 
 
 // result [ 5, 4, 3, 2, 1, 0 ] [ 5, 4, 3, 2, 1, 0 ] count 720 
-// 각 결과들 push 했는데, 왜 죄다 같은 값으로 저장되어있지?
+// 각 결과들 push 했는데, 왜 죄다 같은 값으로 저장되어있지? => 얇은 복사를 했었기에
