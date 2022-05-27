efc52fd0955381e92fcfa25d4f0514db8e6a6304
Marshal1101 <adequateplayer9@gmail.com>
2022-05-27 21:55:49 +0900

    python 폴더생성 이동

--- swjungle-week03/python/x.py
+++ swjungle-week03/python/x.py
@@ -1,13 +1,24 @@
-def changer(list):
-    length = len(list)
-    if length <= 1:
-        return list
-    for i in range(1, length):
-        if list[0] < list[i]:
-            return changer(list[1:i]) + changer(list[i:]) + [list[0]]
-    return changer(list[1:]) + [list[0]]
+a = '7641'
+b = '7621'
 
+c = 1 << 7
+c = c | 1 << 6
+c = c | 1 << 4
+c = c | 1 << 1
 
-list = [50, 30, 24, 5, 28, 45, 98, 52, 60]
+d = 1 << 7
+d = d | 1 << 6
+d = d | 1 << 2
+d = d | 1 << 1
+f = c ^ d
+print(bin(d))
+print(bin(c))
 
-changer(list)
+print(bin(c ^ d))
+print((c ^ d))
+cnt = 0
+for i in range(1, 7) :
+    comp = 1 << i
+    if (f & comp) > 0 :
+        cnt += 1
+print(cnt)
